746663f7eff08f2daa39e78c62a8b9b8947754c0
Harsh-Vaishnani <harshvaishnani8@gmail.com>
2024-11-28 10:25:31 +0530

    FrontEnd

---
+++ JavaScript/variable.js
@@ -0,0 +1,7 @@
+console.log("Hello 𝚅𝚊𝚛𝚒𝚊𝚋𝚕𝚎𝚜");
+
+// In the var variable => Reassign: Allowed     Redeclare: Allowed
+// In the let variable => Reassign: Allowed     Redeclare: Not Allowed
+// In the const variable => Reassign: Not Allowed       Redeclare: Not Allowed 
+
+
